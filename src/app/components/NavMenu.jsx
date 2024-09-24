23d467866bc9a0232b999b68781b734306686aae
Louis Gerber <louisgerber004@gmail.com>
2024-09-24 14:03:32 +0200

    save

--- src/app/components/NavMenu.jsx
+++ src/app/components/NavMenu.jsx
@@ -1,19 +1,38 @@
 "use client";
 
-import Link from "next/link";
+
 import { useState } from "react";
+import { cn } from "../utils";
+
 
 export default function NavMenu() {
   const navElement = ["Projects", "About me", "Contact"];
   const [selectedNavElement, setSelectedNavElement] = useState(0);
+  // const handleKeyPress = () => {
+  //   if("down"){
+  //     if(selectedNavElement < navElement.length - 1){
+  //       setSelectedNavElement(selectedNavElement + 1)
+  //     } else {
+  //       selectedNavElement(0)
+  //     }
+  //   } else if ("down"){
+  //     if(selectedNavElement = 0){
+  //       setSelectedNavElement(navElement.length -1)
+  //     } else {
+  //       selectedNavElement(navElement -1)
+  //     } 
+  //   } else {
+  //     return
+  //   }
+  // }
   return (
-    <div className="">
+    <div className="h-full w-full bg-zinc-100 flex items-center justify-center">
       <nav className=" flex gap-5 flex-col">
         {navElement.map((element, index) => (
-          <div onMouseOver={()=>setSelectedNavElement(index)} className=" flex items-center gap-4 cursor-pointer">
-            {index === selectedNavElement && <span class="material-symbols-outlined">double_arrow</span>}
-            <div className=" text-5xl">{element}</div>
-          </div>
+          <a key={"nav element " + index} href={"/"} onMouseOver={()=>setSelectedNavElement(index)} className=" flex items-center gap-4 cursor-pointer ">
+            <span className={cn("material-symbols-outlined text-zinc-900 customAnim-navChevron", index === selectedNavElement && "customAnim-navChevronOpen")}>double_arrow</span>
+            <div className=" text-8xl text-zinc-900 font-sans antialiased">{element}</div>
+          </a>
         ))}
       </nav>
     </div>
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,17 +1,19 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import Head from "next/head";
+import "./customAnim.css"
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const protestGuerrillaRegular = localFont({
+  src: "./fonts/ProtestGuerrilla-Regular.woff2",
+  variable: "--customFont-1"
 });
+
+
 
 export const metadata = {
   title: "Create Next App",
@@ -21,14 +23,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
       />
+      </head>
 
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${protestGuerrillaRegular.variable}`}>
         {children}
       </body>
     </html>
