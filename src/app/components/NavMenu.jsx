1184446f962fce3ac00be15b217f35b95fc7cf55
Louis Gerber <louisgerber004@gmail.com>
2024-09-25 13:41:57 +0200

    save

--- src/app/components/NavMenu.jsx
+++ src/app/components/NavMenu.jsx
@@ -6,27 +6,27 @@ import { cn } from "../utils";
 
 
 export default function NavMenu() {
-  const navElement = ["Projects", "About me", "Contact"];
+  const navElement = ["Projects", "Stacks","About me", "Contact"];
   const [selectedNavElement, setSelectedNavElement] = useState(0);
-  // const handleKeyPress = () => {
-  //   if("down"){
-  //     if(selectedNavElement < navElement.length - 1){
-  //       setSelectedNavElement(selectedNavElement + 1)
-  //     } else {
-  //       selectedNavElement(0)
-  //     }
-  //   } else if ("down"){
-  //     if(selectedNavElement = 0){
-  //       setSelectedNavElement(navElement.length -1)
-  //     } else {
-  //       selectedNavElement(navElement -1)
-  //     } 
-  //   } else {
-  //     return
-  //   }
-  // }
+  const handleKeyPress = (e) => {
+    if(e.key === "ArrowDown"){
+      if(selectedNavElement < navElement.length - 1){
+        setSelectedNavElement(selectedNavElement + 1)
+      } else {
+        setSelectedNavElement(0)
+      }
+    } else if (e.key === "ArrowUp"){
+      if(selectedNavElement === 0){
+        setSelectedNavElement(navElement.length - 1)
+      } else {
+        setSelectedNavElement(selectedNavElement - 1)
+      } 
+    } else {
+      return
+    }
+  }
   return (
-    <div className="h-full w-full bg-zinc-100 flex items-center justify-center">
+    <div onKeyDown={(e)=>handleKeyPress(e)} className="h-full w-full bg-zinc-100 flex items-center justify-center">
       <nav className=" flex gap-5 flex-col">
         {navElement.map((element, index) => (
           <a key={"nav element " + index} href={"/"} onMouseOver={()=>setSelectedNavElement(index)} className=" flex items-center gap-4 cursor-pointer ">
--- src/app/components/SocialLinks.jsx
+++ src/app/components/SocialLinks.jsx
@@ -1,16 +1,25 @@
-import Image from "next/image"
-import github from "../images/github-icon.svg"
-import linkedin from "../images/linkedin-icon.svg"
+import Image from "next/image";
+import github from "../images/github-icon.svg";
+import linkedin from "../images/linkedin-icon.svg";
 
-export default function SocialLinks(){
-    return(
-        <div className="flex absolute right-4 bottom-4 gap-4">
-            <a href="https://www.youtube.com/">
-                <Image src={github} height={40} width={40} alt="Icon as a link for github" />
-            </a>
-            <a href="https://www.youtube.com/">
-                <Image src={linkedin} height={40} width={40} alt="Icon  as a link for linkedin" />
-            </a>
-        </div>
-    )
+export default function SocialLinks() {
+  return (
+    <div className="flex absolute right-4 bottom-4 gap-4">
+      <SocialLinkElement image={github} link={"/"} />
+      <SocialLinkElement image={linkedin} link={"/"} />
+    </div>
+  );
+}
+
+export function SocialLinkElement({ image, link }) {
+  return (
+    <a href={link} className="transition-all hover:scale-105">
+      <Image
+        src={image}
+        height={40}
+        width={40}
+        alt="Icon as a link for github"
+      />
+    </a>
+  );
 }
