82f6367b5ae47c03a4fd2acc2ef7ea8107deea46
Louis Gerber <louisgerber004@gmail.com>
2024-10-31 20:06:32 +0100

    save

--- src/app/(app pages)/projects/page.jsx
+++ src/app/(app pages)/projects/page.jsx
@@ -1,6 +1,13 @@
+"use client"
+
 import PageTitle from "../../components/PageTitle";
 import data from "../../appdata.json";
 import ImageCarousel from "../../components/ImageCarousel"
+import { useRouter } from "next/navigation";
+import githubIcon from "../../images/projects/github-icon.svg"
+import websiteIcon from "../../images/projects/website-icon.svg"
+import Image from "next/image";
+
 
 export default function Projects() {
   const test = [1, 2, 3];
@@ -57,22 +64,26 @@ export function ProjectBlocContainer({ children }) {
 
 export function ProjectBloc({ projectData }) {
   return (
-    <div className="w-full rounded-3xl h-96 bg-red-500 relative">
-        {/* <ImageCarousel imagesArray={projectData.images}/> */}
+    <div className="w-full overflow-hidden rounded-xl min-h-96 bg-red-500 relative">
+        {projectData.images && <ImageCarousel imagesArray={projectData.images}/>}
       <ProjectStacks
         stacks={projectData.stacks}
         projectName={projectData.name}
       />
+      <ProjectBtnsContainer projectLink={projectData.projectLink} />
     </div>
   );
 }
+
+
+
 
 export function ProjectStacks({ stacks, projectName }) {
   return (
     <div className="absolute top-2 right-2 flex gap-2">
       {stacks.map((stack, index) => (
         <div
-          className="border-solid border-2 py-2 px-4 rounded-full font-black"
+          className="text-zinc-100 border-solid border-2 py-2 px-4 rounded-full font-black bg-zinc-900"
           key={projectName + " stack " + index}
         >
           {stack}
@@ -82,12 +93,35 @@ export function ProjectStacks({ stacks, projectName }) {
   );
 }
 
-export function ProjectBtn({ name, event }) {
+export function ProjectBtnsContainer({projectLink}){
+  const router = useRouter()
+  const redirect = (link) => {
+    router.push(link)
+  }
+  return(
+    <>
+    {projectLink &&  <div className="flex gap-2 absolute bottom-2 right-2">
+        <ProjectBtn name={"Check Code"} event={()=>{redirect(projectLink.github)}} image={githubIcon} />
+        <ProjectBtn name={"Website"} event={()=>{redirect(projectLink.website)}} image={websiteIcon} />
+      </div>}
+    </>
+  )
+}
+
+export function ProjectBtn({ name, event, image }) {
   return (
-    <div
+    <div 
+    className="cursor-pointer flex gap-3 text-zinc-100 border-solid border-2 py-2 px-4 rounded-full font-black bg-zinc-900"
       onClick={() => {
         event();
       }}
-    ></div>
+    >
+      <div>
+        <Image src={image} height={24} width={24} />
+      </div>
+      <div>
+        {name}
+      </div>
+    </div>
   );
 }
--- src/app/components/ImageCarousel.jsx
+++ src/app/components/ImageCarousel.jsx
@@ -2,20 +2,22 @@
 
 import { useState } from "react"
 import Image from "next/image"
+import chevronLeftIcon from "../images/icons/chevronLeft.svg"
+import chevronRightIcon from "../images/icons/chevronRight.svg"
 
 
 export default function ImageCarousel({imagesArray}){
     const [carouselPosition, setCarouselPosition] = useState(0)
     const handleCarouselNavBtnClick = (side) => {
         if(side === "right") {
-            if(carouselPosition < imagesArray.lenght - 1) {
+            if(carouselPosition < imagesArray.length - 1) {
                 setCarouselPosition(carouselPosition + 1)
             } else {
                 setCarouselPosition(0)
             }
         } else if (side === "left") {
             if(carouselPosition === 0) {
-                setCarouselPosition(imagesArray.lenght - 1)
+                setCarouselPosition(imagesArray.length - 1)
             } else {
                 setCarouselPosition(carouselPosition - 1)
             }
@@ -24,10 +26,14 @@ export default function ImageCarousel({imagesArray}){
     return(
         <div className="relative">
             <div className="absolute top-0 left-0 w-full h-full flex justify-between items-center">
-                <div className="px-2" onClick={()=>{handleCarouselNavBtnClick("left")}}>left</div>
-                <div className="px-2" onClick={()=>{handleCarouselNavBtnClick("right")}}>right</div>
+                {/* <div className="px-2" onClick={()=>{handleCarouselNavBtnClick("left")}}>left</div>
+                <div className="px-2" onClick={()=>{handleCarouselNavBtnClick("right")}}>right</div> */}
+                <Image height={80} width={80} className="px-2 cursor-pointer" onClick={()=>{handleCarouselNavBtnClick("left")}} src={chevronLeftIcon} />
+                <Image height={80} width={80} className="px-2 cursor-pointer" onClick={()=>{handleCarouselNavBtnClick("right")}} src={chevronRightIcon} />
+                <div className="absolute left-3 text-3xl z-40 text-white">{carouselPosition}</div>
             </div>
-            <Image fill={true} src={imagesArray[carouselPosition]} />
+            <Image src={imagesArray[carouselPosition]} height={940} width={1900} />
+            <div className="absolute left-3 text-3xl z-40 text-white">{carouselPosition}</div>
         </div>
     )
 }
